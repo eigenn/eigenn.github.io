16f60f459d480f0f4feaa5c2201568cc723dc59a
David Doka <david@idopython.com>
2014-07-23 23:28:47 +0100

    moving to includes.

--- js/controllers.js
+++ js/controllers.js
@@ -3,15 +3,17 @@
 
 var mainEigennApp = angular.module('mainEigennApp', []);
 
-mainEigennApp.controller('ExperienceSectionCtrl', function ($scope, $http) {
+mainEigennApp.controller('ExperienceSectionCtrl', ['$scope', '$http', function ($scope, $http) {
     $http.get('content/experience.json').success(function (data) {
         $scope.experiences = data;
+        $scope.template = "partials/experience.html";
     });
-});
+}]);
 
 
-mainEigennApp.controller('SkillsSectionCtrl', function ($scope, $http) {
+mainEigennApp.controller('SkillsSectionCtrl', ['$scope', '$http', function ($scope, $http) {
     $http.get('content/skills.json').success(function (data) {
         $scope.skills = data;
+        $scope.template = "partials/skills.html";
     });
-});
+}]);
